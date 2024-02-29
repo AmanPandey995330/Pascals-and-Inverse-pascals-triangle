c343db10a74d5edde79c741690b675594d0c917b
Aman Pandey <147979955+Amanpandey2005@users.noreply.github.com>
2024-02-29 18:58:03 +0530

    Update pascals.c

--- pascals.c
+++ pascals.c
@@ -1,28 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-    int i,j ,space, value;
-    printf("how many rows you want to print in pascals triangle ");
-    scanf("%d", &i);
-    for (i = 1; i<=10 ;i++)
-    {
-        for(space = 1; space<= (10-i); space++)
-        {
-            printf("  ");
-        }
-        for(j = 1;j<=i;j++)
-        {
-            if (i == 1||j== 1)
+#include<iostream>
+using namespace std;
 
-            {
-              value = 1  ;
-            }
-           else
-           {
-            value = value*(i-j+1)/(j-1);
-           } 
-           printf("%4d", value);
-        }
-        printf("\n");
+int factorial(int n){
+    if(n<=0){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
     }
 }
+ void printPascalTriangle(int n){
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j <= (n-i-2); j++){
+            cout << "  ";
+        }
+        for(int j = 0; j <= i; j++){ // Use j instead of a
+            cout << factorial(i) / (factorial(j) * factorial(i - j)) << "  ";
+        }
+        cout << endl;
+    }
+}
+int main(){
+    int n;
+    cout << "No of rows in pascal series" ;
+    cin >> n;
+    printPascalTriangle(n);
+}
+ 
